4a97aeb187b9c608fa75e0f7ce9c73357b720928
Khushi-013 <khushi.211583201@vcet.edu.in>
2024-01-02 01:05:39 +0530

    Create Student.java
    Classes & Objects Part-1

---
+++ Student.java
@@ -0,0 +1,49 @@
+public class Student {
+    // Private member variables
+    private String studentName;
+    private int studentId;
+    private double qualifyingMarks;
+    private int yearOfEngineering;
+    private char residentialStatus;
+
+    // Getter and Setter methods
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public double getQualifyingMarks() {
+        return qualifyingMarks;
+    }
+
+    public void setQualifyingMarks(double qualifyingMarks) {
+        this.qualifyingMarks = qualifyingMarks;
+    }
+
+    public int getYearOfEngineering() {
+        return yearOfEngineering;
+    }
+
+    public void setYearOfEngineering(int yearOfEngineering) {
+        this.yearOfEngineering = yearOfEngineering;
+    }
+
+    public char getResidentialStatus() {
+        return residentialStatus;
+    }
+
+    public void setResidentialStatus(char residentialStatus) {
+        this.residentialStatus = residentialStatus;
+    }
+}
